57fc21b39261f2b19ac23361264984c9ee477e7a
lxn <lixiaoning1995@163.com>
2018-08-16 14:13:22 +0800

    写入数据

---
+++ WriteByte/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WriteByte
+{
+    public partial class Form1 : Form
+    {
+        List<byte> data = new List<byte>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            ExportCFG();
+        }
+
+        private void ExportCFG()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "配置文件(*.cfg)|*.cfg"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+
+
+
+
+
+            }
+        }
+    }
+}
