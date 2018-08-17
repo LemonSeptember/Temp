1b934664e106949ae94215b85a951716eb01fb4a
lxn <lixiaoning1995@163.com>
2018-08-17 08:27:22 +0800

    二进制储存文件

--- WriteByte/Form1.Designer.cs
+++ WriteByte/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.buttonSave = new System.Windows.Forms.Button();
             this.buttonExport = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonSave
@@ -51,11 +52,22 @@
             this.buttonExport.UseVisualStyleBackColor = true;
             this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
             // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(90, 285);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 2;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRead);
             this.Controls.Add(this.buttonExport);
             this.Controls.Add(this.buttonSave);
             this.Name = "Form1";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button buttonRead;
     }
 }
 
--- WriteByte/Form1.cs
+++ WriteByte/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,22 @@ namespace WriteByte
 {
     public partial class Form1 : Form
     {
-        List<byte> data = new List<byte>();
+        private List<byte> data = new List<byte>();
 
         public Form1()
         {
             InitializeComponent();
+            ReadData();
         }
+
+        private void ReadData()
+        {
+            for (int i = 0; i <= byte.MaxValue; i++)
+            {
+                data.Add((byte)i);
+            }
+        }
+
 
         private void buttonExport_Click(object sender, EventArgs e)
         {
@@ -34,12 +45,38 @@ namespace WriteByte
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string fileName = saveFileDialog.FileName;
+                Console.WriteLine(fileName);
 
+                using (FileStream stream = File.OpenWrite(fileName))
+                {
+                    using (BinaryWriter writer = new BinaryWriter(stream))
+                    {
+                        foreach (var a in data)
+                        {
+                            writer.Write(a);
+                        }
+                    }
+                }
+                //using (StreamWriter streamWriter = File.CreateText(fileName))
+                //{
+                //    foreach (var a in data)
+                //    {
+                //        streamWriter.Write(a);
+                //    }
+                //}
+                //streamWriter.Close();
+            }
+        }
 
-
-
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < data.Count; i++)
+            {
+                Console.Write(data[i] + "\t");
 
             }
+            Console.WriteLine("\n");
+            Console.WriteLine(data.Count);
         }
     }
 }
