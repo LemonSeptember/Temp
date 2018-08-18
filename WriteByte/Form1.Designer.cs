1e5380c73622aa9939192f1a641bc646f14ce302
lxn <lixiaoning1995@163.com>
2018-08-18 12:30:03 +0800

    Byte转换Bit

--- WriteByte/Form1.Designer.cs
+++ WriteByte/Form1.Designer.cs
@@ -31,11 +31,18 @@
             this.buttonSave = new System.Windows.Forms.Button();
             this.buttonExport = new System.Windows.Forms.Button();
             this.buttonRead = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxByte = new System.Windows.Forms.TextBox();
+            this.textBoxBit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonbuttonByteToBit = new System.Windows.Forms.Button();
+            this.buttonBitToByte = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(417, 381);
+            this.buttonSave.Location = new System.Drawing.Point(418, 232);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 0;
@@ -44,7 +51,7 @@
             // 
             // buttonExport
             // 
-            this.buttonExport.Location = new System.Drawing.Point(90, 381);
+            this.buttonExport.Location = new System.Drawing.Point(206, 232);
             this.buttonExport.Name = "buttonExport";
             this.buttonExport.Size = new System.Drawing.Size(75, 23);
             this.buttonExport.TabIndex = 1;
@@ -54,7 +61,7 @@
             // 
             // buttonRead
             // 
-            this.buttonRead.Location = new System.Drawing.Point(90, 285);
+            this.buttonRead.Location = new System.Drawing.Point(12, 232);
             this.buttonRead.Name = "buttonRead";
             this.buttonRead.Size = new System.Drawing.Size(75, 23);
             this.buttonRead.TabIndex = 2;
@@ -62,17 +69,87 @@
             this.buttonRead.UseVisualStyleBackColor = true;
             this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(481, 201);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBoxByte
+            // 
+            this.textBoxByte.Location = new System.Drawing.Point(12, 321);
+            this.textBoxByte.Name = "textBoxByte";
+            this.textBoxByte.Size = new System.Drawing.Size(100, 21);
+            this.textBoxByte.TabIndex = 4;
+            this.textBoxByte.Text = "1";
+            // 
+            // textBoxBit
+            // 
+            this.textBoxBit.Location = new System.Drawing.Point(12, 364);
+            this.textBoxBit.Name = "textBoxBit";
+            this.textBoxBit.Size = new System.Drawing.Size(100, 21);
+            this.textBoxBit.TabIndex = 5;
+            this.textBoxBit.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Byte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Bit";
+            // 
+            // buttonbuttonByteToBit
+            // 
+            this.buttonbuttonByteToBit.Location = new System.Drawing.Point(143, 319);
+            this.buttonbuttonByteToBit.Name = "buttonbuttonByteToBit";
+            this.buttonbuttonByteToBit.Size = new System.Drawing.Size(75, 23);
+            this.buttonbuttonByteToBit.TabIndex = 8;
+            this.buttonbuttonByteToBit.Text = "ByteToBit";
+            this.buttonbuttonByteToBit.UseVisualStyleBackColor = true;
+            this.buttonbuttonByteToBit.Click += new System.EventHandler(this.buttonbuttonByteToBit_Click);
+            // 
+            // buttonBitToByte
+            // 
+            this.buttonBitToByte.Location = new System.Drawing.Point(143, 362);
+            this.buttonBitToByte.Name = "buttonBitToByte";
+            this.buttonBitToByte.Size = new System.Drawing.Size(75, 23);
+            this.buttonBitToByte.TabIndex = 9;
+            this.buttonBitToByte.Text = "BitToByte";
+            this.buttonBitToByte.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonBitToByte);
+            this.Controls.Add(this.buttonbuttonByteToBit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBit);
+            this.Controls.Add(this.textBoxByte);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonRead);
             this.Controls.Add(this.buttonExport);
             this.Controls.Add(this.buttonSave);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +158,13 @@
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.Button buttonExport;
         private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxByte;
+        private System.Windows.Forms.TextBox textBoxBit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonbuttonByteToBit;
+        private System.Windows.Forms.Button buttonBitToByte;
     }
 }
 
--- WriteByte/Form1.cs
+++ WriteByte/Form1.cs
@@ -13,7 +13,8 @@ namespace WriteByte
 {
     public partial class Form1 : Form
     {
-        private List<byte> data = new List<byte>();
+        private const int MAX_VALUE = 512;
+        private byte[] data = new byte[MAX_VALUE];
 
         public Form1()
         {
@@ -25,8 +26,15 @@ namespace WriteByte
         {
             for (int i = 0; i <= byte.MaxValue; i++)
             {
-                data.Add((byte)i);
+                data[i] = (byte)i;
             }
+            string name = "cnfg";
+            char[] array = name.ToCharArray();
+            for (int i = 0; i < name.Length; i++)
+            {
+                data[i+256] = (byte)array[i];
+            }
+
         }
 
 
@@ -70,13 +78,48 @@ namespace WriteByte
 
         private void buttonRead_Click(object sender, EventArgs e)
         {
-            for(int i = 0; i < data.Count; i++)
+            foreach (var temp in data)
             {
-                Console.Write(data[i] + "\t");
-
+                textBox1.AppendText(temp.ToString() + "\t");
             }
-            Console.WriteLine("\n");
-            Console.WriteLine(data.Count);
+            textBox1.AppendText(Environment.NewLine + Environment.NewLine + data.Length);
+            
+        }
+
+
+        private byte BitToByte(byte[] array)
+        {
+            byte @byte=0;
+            for(int i = 0; i < 8; i++)
+            {
+                @byte += (byte)(2 * i * array[i]);
+            }
+            return @byte;
+        }
+
+        private void buttonbuttonByteToBit_Click(object sender, EventArgs e)
+        {
+            textBoxBit.Clear();
+            
+            byte temp = byte.Parse(textBoxByte.Text);
+            byte[] text = ByteToBit(temp);
+            for (int i = text.Length-1; i >=0 ; i--)
+                textBoxBit.AppendText(text[i].ToString());
+
+        }
+
+        private byte[] ByteToBit(byte @byte)
+         {
+            byte[] array = new byte[8];
+            for (int i = 0; i <= 7; i++)
+            {
+                array[i] = (byte)(@byte & 0x01);
+                @byte = (byte)(@byte >> 1);
+            }
+
+            return array;
+
+
         }
     }
 }
