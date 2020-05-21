f7879cae2802366c4d35b13d8f3164382107c7dc
lxn <lixiaoning1995@163.com>
2020-05-21 11:30:44 +0800

    2020年5月21日

---
+++ BlockingCollection/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockingCollection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            var queue = new ConcurrentQueue<string>();
+            // 生产者线程
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    queue.Enqueue("value" + count);
+                    count++;
+                }
+            });
+            // 消费者线程1
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    string value;
+                    if (queue.TryDequeue(out value))
+                    {
+                        Console.WriteLine("Worker 1: " + value);
+                    }
+                }
+            });
+            // 消费者线程2
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    string value;
+                    if (queue.TryDequeue(out value))
+                    {
+                        Console.WriteLine("Worker 2: " + value);
+                    }
+
+                }
+            });
+
+
+            Thread.Sleep(50000);
+
+            Console.ReadLine();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            var blockingCollection = new BlockingCollection<string>(500);
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    blockingCollection.Add("value" + count);
+                    count++;
+                }
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    //Console.WriteLine("Worker 1: " + blockingCollection.Take());
+                    richTextBox1.AppendText("Worker 1: " + blockingCollection.Take());
+                }
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    //Console.WriteLine("Worker 2: " + blockingCollection.Take());
+                    richTextBox2.AppendText("Worker 2: " + blockingCollection.Take());
+                }
+            });
+
+            Thread.Sleep(50000);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TimerTest/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimerTest
+{
+    public partial class Form1 : Form
+    {
+        DateTime dtFrom = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        int timeValue = 0;
+
+        BlockingCollection<string> ExportStr = new BlockingCollection<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = mTimer.Interval;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            mTimer.Interval = (int)numericUpDown1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExportStr = new BlockingCollection<string>();
+            mTimer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mTimer.Stop();
+            //timeValue = 0;
+        }
+
+        private void mTimer_Tick(object sender, EventArgs e)
+        {
+            //timeValue++;
+            //Console.WriteLine(timeValue);
+            //Console.WriteLine((DateTime.Now.ToUniversalTime().Ticks - dtFrom.Ticks) / 10000);
+            //Console.WriteLine(DateTime.Now.ToString("mm:ss.ffff"));
+            ExportStr.Add(DateTime.Now.ToString("mm:ss.ffff"));
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.FileName = DateTime.Now.ToString("mm_ss");
+                saveFileDialog.Filter = "TXT|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                    {
+                        using (StreamWriter sw = new StreamWriter(fileStream))
+                        {
+                            foreach (var item in ExportStr)
+                            {
+                                sw.WriteLine(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
