78d947350998ec9c9c5cf409584ddbfa2346ecd6
lxn <lixiaoning1995@163.com>
2020-12-19 15:16:27 +0800

    2020年12月19日

---
+++ DrawBezierTest/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawBezierTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBeziersPointF(e);
+        }
+        private void DrawBezierFloat(PaintEventArgs e)
+        {
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black, 3);
+            Pen redPen = new Pen(Color.Red, 1);
+
+            // Create coordinates of points for curve.
+            float startX = 100.0F;
+            float startY = 100.0F;
+            float controlX1 = 200.0F;
+            float controlY1 = 10.0F;
+            float controlX2 = 350.0F;
+            float controlY2 = 50.0F;
+            float endX = 500.0F;
+            float endY = 100.0F;
+
+            // Draw arc to screen.
+            e.Graphics.DrawBezier(blackPen, startX, startY,
+                controlX1, controlY1,
+                controlX2, controlY2,
+                endX, endY);
+            e.Graphics.DrawLines(redPen, new PointF[] { new PointF(startX, startY), new PointF(controlX1, controlY1), new PointF(controlX2, controlY2), new PointF(endX, endY) });
+        }
+        private void DrawBeziersPointF(PaintEventArgs e)
+        {
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black, 3);
+            Pen redPen = new Pen(Color.Red, 1);
+
+            // Create points for curve.
+            PointF start = new PointF(100.0F, 100.0F);
+            PointF control1 = new PointF(200.0F, 10.0F);
+            PointF control2 = new PointF(350.0F, 50.0F);
+            PointF end1 = new PointF(500.0F, 100.0F);
+            PointF control3 = new PointF(600.0F, 150.0F);
+            PointF control4 = new PointF(650.0F, 250.0F);
+            PointF end2 = new PointF(500.0F, 300.0F);
+            PointF[] bezierPoints = { start, control1, control2, end1,
+         control3, control4, end2 };
+
+            // Draw arc to screen.
+            e.Graphics.DrawBeziers(blackPen, bezierPoints);
+            e.Graphics.DrawLines(redPen, bezierPoints);
+        }
+    }
+}
---
+++ SwitchTimeTest/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SwitchTimeTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// switch
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("执行");
+                        break;
+                    case 1:
+                        Console.WriteLine("执行");
+                        break;
+                    case 2:
+                        Console.WriteLine("执行");
+                        break;
+                    case 3:
+                        Console.WriteLine("执行");
+                        break;
+                    default:
+                        Console.WriteLine("执行");
+                        break;
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine("switch: " + stopWatch.ElapsedMilliseconds);
+        }
+
+        /// <summary>
+        /// if
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("执行");
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("执行");
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("执行");
+                }
+                else if (i == 3)
+                {
+                    Console.WriteLine("执行");
+                }
+                else
+                {
+                    Console.WriteLine("执行");
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine("if: " + stopWatch.ElapsedMilliseconds);
+        }
+    }
+}
