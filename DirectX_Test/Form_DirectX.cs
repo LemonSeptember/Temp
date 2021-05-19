9e13dab052c75a08eb79ed7a803995ce8056c39a
Lemon <1318324614@qq.com>
2021-05-19 15:55:01 +0800

    2021年5月19日15:54:57

--- DirectX_Test/Form_DirectX.cs
+++ DirectX_Test/Form_DirectX.cs
@@ -15,18 +15,18 @@ namespace DirectX_Test
 
         private void Form_DirectX_Load(object sender, EventArgs e)
         {
-            
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
             string path = Path.Combine(@"D:\Users\KETIZU2\Desktop\dll\alarm", "S250HZ.wav");
             DxSpeaker.SetMediaPath(path);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             DxSpeaker.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DxSpeaker.Stop();
         }
     }
 }
